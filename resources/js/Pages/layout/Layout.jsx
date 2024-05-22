b6d0328415677f5725fbf73265de9816b7d38257
agilalatas <agilalatas17@gmail.com>
2024-05-22 20:01:05 +0700

    install and config inertia react

---
+++ resources/js/Pages/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import { Link } from '@inertiajs/react'
+
+function Layout({ children }) {
+  return (
+    <main>
+      <header>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </header>
+      <article>{children}</article>
+    </main>
+  )
+}
+
+export default Layout
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/users', [UserController::class, 'show'])->name('users.index');
